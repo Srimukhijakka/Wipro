845b9b278351acebad3be9609b27c1c7475ec2c7
github_Srimukhi jakka <srimukhijakka@gmail.com>
2020-06-21 20:25:36 -0400

    commit

---
+++ Sunday/src/com/Example33.java
@@ -0,0 +1,32 @@
+package com;
+
+public class Example33 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a=new int[] {1,10,10,2,10};
+		int n=a.length,k=0,c=0;
+		int[] r=new int[] {0,0,0,0,0};
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]==10)
+				c++;
+		}
+		while(k<n && k<(n-c))
+		{
+		for(int i=0;i<n;i++)
+		{
+				if(a[i]!=10)
+				{
+					r[k]=a[i];
+					k++;
+				}
+		}
+		}	
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(r[i]+" ");
+		}
+	}
+
+}
